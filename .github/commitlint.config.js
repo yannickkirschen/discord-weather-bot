3152271f604e66580f6ab851fb1c6c503493368c
Yannick Kirschen <yannickkirschen@protonmail.com>
2023-08-25 07:31:51 +0200

    chore(ci): increase max body length in git commit

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     rules: {
         'body-leading-blank': [2, 'always'],
-        'body-max-line-length': [2, 'always', 100],
+        'body-max-line-length': [2, 'always', 140],
         'header-max-length': [2, 'always', 100],
         'scope-case': [2, 'always', 'lower-case'],
         'subject-case': [
